45f5c86bfcbbe8550a04fa6eb35d25839d5e14c2
Marc Jakobi <marc.jakobi@velasolaris.com>
2021-12-22 00:00:49 +0100

    Modifactions for NixOS compatibility

--- .config/nvim/lua/lspconfig-setup.lua
+++ .config/nvim/lua/lspconfig-setup.lua
@@ -120,11 +120,13 @@ lspconfig.cmake.setup{ on_attach = on_attach }
 lspconfig.gopls.setup{ on_attach = on_attach }
 lspconfig.vimls.setup{ on_attach = on_attach }
 
-local sumneko_root_path = os.getenv("HOME") .. '/git/clones/lua-language-server'
-local sumneko_binary = sumneko_root_path.."/bin/Linux/lua-language-server"
+-- local sumneko_root_path = os.getenv("HOME") .. '/git/clones/lua-language-server'
+-- local sumneko_binary = sumneko_root_path.."/bin/Linux/lua-language-server"
+local sumneko_binary = "lua-language-server"
 local luadev = require("lua-dev").setup({
   lspconfig = {
-    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+    -- cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"};
+    cmd = { sumneko_binary };
     settings = {
       Lua = {
         runtime = {
@@ -153,7 +155,7 @@ local luadev = require("lua-dev").setup({
     on_attach = on_attach,
   }
 })
--- lspconfig.sumneko_lua.setup(luadev)
+lspconfig.sumneko_lua.setup(luadev)
 require('nlua.lsp.nvim').setup(lspconfig, {
   on_attach = on_attach,
 })
@@ -299,4 +301,3 @@ vim.api.nvim_exec([[
 
 -- nvim-dap-virtual-text plugin
 require'nvim-dap-virtual-text'.setup()
-
