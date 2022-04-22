4acb430308dfab89d9e8d66c6e9e2158f7652f4d
Marc Jakobi <marc.jakobi@tiko.energy>
2022-04-22 12:55:31 +0200

    Update nvim plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -320,25 +320,47 @@ return require('packer').startup(function(use)
     },
     config = function()
       require('gitsigns').setup({
-          keymaps = {
-            -- Default keymap options
-            noremap = true,
-            ['n ]g'] = { expr = true, "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'"},
-            ['n [g'] = { expr = true, "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'"},
-            ['n <leader>hs'] = '<cmd>Gitsigns stage_hunk<CR>',
-            ['v <leader>hs'] = ':Gitsigns stage_hunk<CR>',
-            ['n <leader>hu'] = '<cmd>Gitsigns undo_stage_hunk<CR>',
-            ['n <leader>hr'] = '<cmd>Gitsigns reset_hunk<CR>',
-            ['v <leader>hr'] = ':Gitsigns reset_hunk<CR>',
-            ['n <leader>hR'] = '<cmd>Gitsigns reset_buffer<CR>',
-            ['n <leader>hp'] = '<cmd>Gitsigns preview_hunk<CR>',
-            ['n <leader>hb'] = '<cmd>lua require"gitsigns".blame_line{full=true}<CR>',
-            ['n <leader>hS'] = '<cmd>Gitsigns stage_buffer<CR>',
-            ['n <leader>hU'] = '<cmd>Gitsigns reset_buffer_index<CR>',
-            -- Text objects
-            ['o ih'] = ':<C-U>Gitsigns select_hunk<CR>',
-            ['x ih'] = ':<C-U>Gitsigns select_hunk<CR>'
+          current_line_blame = true,
+          current_line_blame_opts = {
+            ignore_whitespace = true,
           },
+          on_attach = function(bufnr)
+            local gs = package.loaded.gitsigns
+
+            local function map(mode, l, r, opts)
+              opts = opts or {}
+              opts.buffer = bufnr
+              vim.keymap.set(mode, l, r, opts)
+            end
+
+            -- Navigation
+            map('n', ']g', function()
+              if vim.wo.diff then return ']g' end
+              vim.schedule(function() gs.next_hunk() end)
+              return '<Ignore>'
+            end, {expr=true})
+
+            map('n', '[g', function()
+              if vim.wo.diff then return '[g' end
+              vim.schedule(function() gs.prev_hunk() end)
+              return '<Ignore>'
+            end, {expr=true})
+
+            -- Actions
+            map({'n', 'v'}, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+            map({'n', 'v'}, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+            map('n', '<leader>hS', gs.stage_buffer)
+            map('n', '<leader>hu', gs.undo_stage_hunk)
+            map('n', '<leader>hR', gs.reset_buffer)
+            map('n', '<leader>hp', gs.preview_hunk)
+            map('n', '<leader>hb', function() gs.blame_line{full=true} end)
+            map('n', '<leader>tb', gs.toggle_current_line_blame)
+            map('n', '<leader>hd', gs.diffthis)
+            map('n', '<leader>hD', function() gs.diffthis('~') end)
+            map('n', '<leader>td', gs.toggle_deleted)
+            -- Text object
+            map({'o', 'x'}, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+          end,
         })
     end
   }
