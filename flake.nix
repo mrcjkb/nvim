cf8efd1ae36923f357354d841ba6f1463d258d95
Marc Jakobi <marc.jakobi@tiko.energy>
2022-08-08 20:31:42 +0200

    Add sqlite env vars

--- flake.nix
+++ flake.nix
@@ -23,40 +23,46 @@
         ];
       };
 
-      environment.systemPackages = with pkgs; [
-        neovim
-        unstable.neovim-remote
-        unstable.tree-sitter
-        unstable.sqlite
-        (unstable.lua.withPackages (luapkgs: with luapkgs; [
-                                    luacheck
-                                    plenary-nvim
-                                    luacov
-        ]))
-        unstable.haskellPackages.hoogle
-        unstable.haskellPackages.hlint
-        unstable.haskell-language-server
-        unstable.haskellPackages.haskell-debug-adapter
-        unstable.stylish-haskell
-        unstable.rnix-lsp # Nix language server
-        unstable.rust-analyzer
-        unstable.sumneko-lua-language-server
-        unstable.nodePackages.vim-language-server
-        unstable.nodePackages.yaml-language-server
-        unstable.nodePackages.dockerfile-language-server-nodejs
-        unstable.glow # Render markdown on the command-line
-        unstable.bat # cat with syntax highlighting
-        unstable.ueberzug # Display images in terminal
-        unstable.feh # Fast and light image viewer
-        unstable.fzf # Fuzzy search
-        unstable.xclip # Required so that neovim compiles with clipboard support
-        unstable.ripgrep # Fast (Rust) re-implementation of grep
-        unstable.fd # Fast alternative to find
-        unstable.jdt-language-server
-        unstable.nodePackages.yarn # Required by markdown-preview vim plugin
-        python-language-server
-        unstable.nodePackages.pyright
-      ];
+      environment = {
+        systemPackages = with pkgs; [
+          neovim
+          unstable.neovim-remote
+          unstable.tree-sitter
+          unstable.sqlite
+          (unstable.lua.withPackages (luapkgs: with luapkgs; [
+                                      luacheck
+                                      plenary-nvim
+                                      luacov
+          ]))
+          unstable.haskellPackages.hoogle
+          unstable.haskellPackages.hlint
+          unstable.haskell-language-server
+          unstable.haskellPackages.haskell-debug-adapter
+          unstable.stylish-haskell
+          unstable.rnix-lsp # Nix language server
+          unstable.rust-analyzer
+          unstable.sumneko-lua-language-server
+          unstable.nodePackages.vim-language-server
+          unstable.nodePackages.yaml-language-server
+          unstable.nodePackages.dockerfile-language-server-nodejs
+          unstable.glow # Render markdown on the command-line
+          unstable.bat # cat with syntax highlighting
+          unstable.ueberzug # Display images in terminal
+          unstable.feh # Fast and light image viewer
+          unstable.fzf # Fuzzy search
+          unstable.xclip # Required so that neovim compiles with clipboard support
+          unstable.ripgrep # Fast (Rust) re-implementation of grep
+          unstable.fd # Fast alternative to find
+          unstable.jdt-language-server
+          unstable.nodePackages.yarn # Required by markdown-preview vim plugin
+          python-language-server
+          unstable.nodePackages.pyright
+        ];
+      };
+      sessionVariables = rec {
+        LIBSQLITE_CLIB_PATH = "${unstable.sqlite.out}/lib/libsqlite3.so";
+        LIBSQLITE = LIBSQLITE_CLIB_PATH; # Expected by sqlite plugin
+      };
     };
   };
 }
