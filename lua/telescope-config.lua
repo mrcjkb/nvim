009b09ad40d0aaeaed13954efc83254b68e64af9
Marc Jakobi <marc.jakobi@tiko.energy>
2022-08-17 20:36:42 +0200

    Update telescope-config to use vim.keymap.set

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -1,9 +1,7 @@
 local telescope = require'telescope'
 local actions = require'telescope.actions'
-local api = vim.api
 
 local M = {}
-
 -- Fall back to find_files if not in a git repo
 M.project_files = function()
   local opts = {} -- define here if you want to define something
@@ -12,25 +10,24 @@ M.project_files = function()
 end
 
 
-local opts = { noremap=true, silent=true }
-api.nvim_set_keymap('n', '<C-p>', '<Cmd>Telescope find_files<CR>', opts)
-api.nvim_set_keymap('n', '<C-g>', '<Cmd>Telescope live_grep<CR>', opts) 
+vim.keymap.set('n', '<C-p>', telescope.builtin.find_files, { })
+vim.keymap.set('n', '<C-g>', telescope.builtin.live_grep, { })
 -- TODO: Add live_grpe call with vimgrep_arguments that filters --type for the current file type
 -- @see https://github.com/nvim-telescope/telescope.nvim/blob/ad32a4c4535af9feae1f16129de22cfc57921ef8/lua/telescope/builtin/__files.lua
-api.nvim_set_keymap('n', '<leader>*', '<Cmd>Telescope grep_string<CR>', opts) -- Search for string under the cursor 
-api.nvim_set_keymap('n', '<leader>tg', "<CMD>lua require'telescope-config'.project_files()<CR>", opts)
-api.nvim_set_keymap('n', '<leader>tc', '<Cmd>Telescope quickfix<CR>', opts)
-api.nvim_set_keymap('n', '<leader>tq', '<Cmd>Telescope command_history<CR>', opts)
-api.nvim_set_keymap('n', '<leader>tl', '<Cmd>Telescope loclist<CR>', opts)
-api.nvim_set_keymap('n', '<leader>tr', '<Cmd>Telescope registers<CR>', opts)
-api.nvim_set_keymap('n', '<leader>tp', '<Cmd>Telescope repo list<CR>', opts)
-api.nvim_set_keymap('n', '<leader>tb', '<Cmd>Telescope buffers<CR>', opts)
-api.nvim_set_keymap('n', '<leader>tf', '<Cmd>Telescope current_buffer_fuzzy_find<CR>', opts)
-api.nvim_set_keymap('n', '<leader>td', '<Cmd>Telescope lsp_document_symbols<CR>', opts)
-api.nvim_set_keymap('n', '<leader>ta', '<Cmd>Telescope lsp_code_actions<CR>', opts)
-api.nvim_set_keymap('n', '<leader>ts', '<Cmd>Telescope ultisnips ultisnips<CR>', opts)
-api.nvim_set_keymap('n', '<leader>th', '<Cmd>Telescope hoogle<CR>', opts)
-api.nvim_set_keymap('n', '<leader>to', '<Cmd>lua require\'telescope.builtin\'.lsp_dynamic_workspace_symbols{}<CR>', opts)
+vim.keymap.set('n', '<leader>*', telescope.builtin.grep_string, { })
+vim.keymap.set('n', '<leader>tg', require'telescope-config'.project_files, { })
+vim.keymap.set('n', '<leader>tc', telescope.builtin.quickfix, { })
+vim.keymap.set('n', '<leader>tq', telescope.builtin.command_history, { })
+vim.keymap.set('n', '<leader>tl', telescope.builtin.loclist, { })
+vim.keymap.set('n', '<leader>tr', telescope.builtin.registers, { })
+vim.keymap.set('n', '<leader>tp', '<Cmd>Telescope repo list<CR>', { })
+vim.keymap.set('n', '<leader>tb', telescope.builtin.buffers, { })
+vim.keymap.set('n', '<leader>tf', telescope.builtin.current_buffer_fuzzy_find, { })
+vim.keymap.set('n', '<leader>td', telescope.builtin.lsp_document_symbols, { })
+vim.keymap.set('n', '<leader>ta', telescope.builtin.lsp_code_actions, { })
+vim.keymap.set('n', '<leader>ts', '<Cmd>Telescope ultisnips ultisnips<CR>', { })
+vim.keymap.set('n', '<leader>th', '<Cmd>Telescope hoogle<CR>', { })
+vim.keymap.set('n', '<leader>to', telescope.builtin.lsp_dynamic_workspace_symbols, { })
 -- api.nvim_set_keymap('n', '<leader>to', '<Cmd>Telescope lsp_workspace_symbols<CR>', opts)
 
 -- Don't preview binaries
