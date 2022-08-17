c03afb770b5c9178b62490976731677001cb052f
Marc Jakobi <marc.jakobi@tiko.energy>
2022-08-17 22:01:35 +0200

    Use default telescope previoewer

--- lua/telescope-config.lua
+++ lua/telescope-config.lua
@@ -42,29 +42,6 @@ vim.keymap.set('n', '<leader>th', '<Cmd>Telescope hoogle<CR>', { })
 vim.keymap.set('n', '<leader>to', builtin.lsp_dynamic_workspace_symbols, { })
 -- api.nvim_set_keymap('n', '<leader>to', '<Cmd>Telescope lsp_workspace_symbols<CR>', opts)
 
--- Don't preview binaries
-local previewers = telescope.previewers
-local Job = require('plenary.job')
-local new_maker = function(filepath, bufnr, opts)
-  filepath = vim.fn.expand(filepath)
-  Job:new({
-    command = 'file',
-    args = { '--mime-type', '-b', filepath },
-    on_exit = function(j)
-      local mime_type = vim.split(j:result()[1], '/')[1]
-      if mime_type == 'text' then
-        previewers.buffer_previewer_maker(filepath, bufnr, opts)
-      else
-        -- maybe we want to write something to the buffer here
-        vim.schedule(function()
-          vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, { 'BINARY' })
-        end)
-      end
-    end
-  }):sync()
-end
-
-
 telescope.setup {
   defaults = {
     path_display = { 
@@ -84,7 +61,6 @@ telescope.setup {
     preview = {
       treesitter = true
     },
-    buffer_previewer_maker = new_maker,
     history = {
       path = vim.fn.stdpath('data') .. '/telescope_history.sqlite3',
       limit = 1000,
