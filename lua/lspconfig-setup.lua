33ca2eb990bd8f37969deab2f018c8b319228618
Marc Jakobi <marc.jakobi@tiko.energy>
2022-09-30 11:03:01 +0200

    Add all defaults to hls setup

--- lua/lspconfig-setup.lua
+++ lua/lspconfig-setup.lua
@@ -95,8 +95,79 @@ lspconfig.hls.setup{
       formattingProvider = 'stylish-haskell',
       checkProject = false,
       plugin = {
+        alternateNumberFormat = {globalOn = true,},
+        callHierarchy = {globalOn = true,},
+        changeTypeSignature = {globalOn = true,},
+        class = { 
+          codeActionsOn = true, 
+          codeLensOn = true, 
+        },
+        eval = {
+          globalOn = true,
+          config = {
+            diff = true,
+            exception = true,
+          },
+        },
+        excplicitFixity = {globalOn = true,},
+        gadt = {globalOn = true,},
+        ['ghcide-code-actions-bindings'] = {globalOn = true,},
+        ['ghcide-code-actions-fill-holes'] = {globalOn = true,},
+        ['ghcide-code-actions-imports-exports'] = {globalOn = true,},
+        ['ghcide-code-actions-type-signatures'] = {globalOn = true,},
+        ['ghcide-completions'] = {
+          globalOn = true,
+          config = {
+            autoExtendOn = true,
+            snippetsOn = true,
+          },
+        },
+        ['ghcide-hover-and-symbols'] = {
+          hoverOn = true,
+          symbolsOn = true,
+        },
+        ['ghcide-type-lenses'] = {
+          globalOn = true,
+          config = {
+            mode = 'always',
+          },
+        },
+        haddockComments = {globalOn = true,},
+        hlint = {
+          codeActionsOn = true,
+          diagnosticsOn = true,
+        },
+        importLens = {
+          globalOn = true,
+          codeLensOn = true,
+        },
+        moduleName = {globalOn = true,},
+        pragmas = {
+          codeActionsOn = true,
+          completionOn = true,
+        },
+        qualifyImportedNames = {globalOn = true,},
+        refineImports = {
+          codeActionsOn = true,
+          codeLensOn = true,
+        },
         rename = {
-          globalOn = true;
+          globalOn = true,
+          config = {crossModule = true,},
+        },
+        retrie = {globalOn = true,},
+        splice = {globalOn = true,},
+        tactics = {
+          codeActionsOn = true,
+          codeLensOn = true,
+          config = {
+            auto_gas = 4,
+            hole_severity = nil,
+            max_use_ctor_actions = 5,
+            proofstate_styling = true,
+            timeout_duration = 2,
+          },
+          hoverOn = true,
         },
       },
     },
