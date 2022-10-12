6ecf2ef10d4a2cd4b315f11c123e46e93a48503f
Marc Jakobi <marc.jakobi@tiko.energy>
2022-10-12 21:35:29 +0200

    Add neotest icons to replace vscode icons

--- lua/neotest-config.lua
+++ lua/neotest-config.lua
@@ -1,4 +1,4 @@
-require('neotest').setup({
+require('neotest').setup {
   adapters = {
     require("neotest-haskell"),
     require("neotest-python")({
@@ -9,8 +9,14 @@ require('neotest').setup({
     -- require("neotest-vim-test")({
       --   ignore_file_types = { "haskell", "python", "vim", "lua", "rust" },
       -- }),
-    },
-  })
+  },
+  icons = {
+    failed = '✘',
+    passed = '✔',
+    running = '🏃',
+    skipped = '⏭',
+  }
+}
   local opts = { noremap = true, }
   vim.keymap.set('n', '<leader>nr', function() require('neotest').run.run() end, opts)
   vim.keymap.set('n', '<leader>no', function() require('neotest').output.open() end, opts)
