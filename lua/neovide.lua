5222fdf05df5ec39b75af2f55fdaa9ef834b7ee0
Marc Jakobi <marc.jakobi@tiko.energy>
2022-10-13 21:37:41 +0200

    Add neovide configs

---
+++ lua/neovide.lua
@@ -0,0 +1,9 @@
+if not vim.g.neovide then
+  return
+end
+
+vim.o.guifont = 'JetBrains mono:h16'
+vim.g.neovide_hide_mouse_when_typing = true
+vim.g.neovide_underline_automatic_scaling = true -- Noticeable for font sizes above 15
+
+
