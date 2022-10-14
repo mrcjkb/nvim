74c355558616c9787470956e565fdf75c221e947
Marc Jakobi <marc.jakobi@tiko.energy>
2022-10-14 14:20:26 +0200

    Format

--- lua/lualine-setup.lua
+++ lua/lualine-setup.lua
@@ -3,48 +3,48 @@ gps.setup()
 require("lualine").setup({
         globalstatus = true;
         sections = {
-            lualine_c = {
-                { 
-                    'filename',
-                    path = 1,
-                },
-                { gps.get_location, cond = gps.is_available },
+          lualine_c = {
+            { 
+              'filename',
+              path = 1,
             },
+            { gps.get_location, cond = gps.is_available },
+          },
         },
         options = { 
-            theme = "material",
+          theme = "material",
         },
         tabline = {
-            lualine_a = {
-                {
-                    'tabs',
-                    mode = 1,
-                }
+          lualine_a = {
+            {
+              'tabs',
+              mode = 1,
+            }
+          },
+          lualine_b = {
+            {
+              'buffers',
+              show_filename_only = true,
+              show_bufnr = true,
+              mode = 4,
+              filetype_names = {
+                TelescopePrompt = 'Telescope',
+                dashboard = 'Dashboard',
+                packer = 'Packer',
+                fzf = 'FZF',
+                alpha = 'Alpha'
+              },
+              buffers_color = {
+                -- Same values as the general color option can be used here.
+                active = 'lualine_b_normal',     -- Color for active buffer.
+                inactive = 'lualine_b_inactive', -- Color for inactive buffer.
+              },
             },
-            lualine_b = {
-                {
-                    'buffers',
-                    show_filename_only = true,
-                    show_bufnr = true,
-                    mode = 4,
-                    filetype_names = {
-                        TelescopePrompt = 'Telescope',
-                        dashboard = 'Dashboard',
-                        packer = 'Packer',
-                        fzf = 'FZF',
-                        alpha = 'Alpha'
-                    },
-                    buffers_color = {
-                        -- Same values as the general color option can be used here.
-                        active = 'lualine_b_normal',     -- Color for active buffer.
-                        inactive = 'lualine_b_inactive', -- Color for inactive buffer.
-                    },
-                },
-            },
-            lualine_c = {},
-            lualine_x = {},
-            lualine_y = {},
-            lualine_z = {}
+          },
+          lualine_c = {},
+          lualine_x = {},
+          lualine_y = {},
+          lualine_z = {}
         },
         extenstions = {'fugitive', 'fzf', 'toggleterm', 'quickfix'}
-    })
+      })
