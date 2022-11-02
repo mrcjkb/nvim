9cef6fea782e5cd41c2444f3b792b003168a98e6
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-02 23:04:10 +0100

    set codelens hightlight italic

--- lua/highlights.lua
+++ lua/highlights.lua
@@ -3,5 +3,5 @@ vim.api.nvim_set_hl(0, 'TermCursor', {link = 'Cursor',})
 vim.api.nvim_set_hl(0, 'TermCursorNC', {bg='red', fg='white', ctermbg=1, ctermfg=15})
 
 
-vim.api.nvim_set_hl(0, 'LspCodeLens', { fg = '#BC96da', underline=false, })
+vim.api.nvim_set_hl(0, 'LspCodeLens', { fg = '#BC96da', underline=false, italic = true, })
 
