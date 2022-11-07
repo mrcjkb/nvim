32d00cd0cf1e6db9ca8bd2ebf424800e5311f484
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-07 18:48:17 +0100

    set repl_open_cmd to 1/3 of line count

--- lua/repl-setup.lua
+++ lua/repl-setup.lua
@@ -13,7 +13,7 @@ iron.setup {
     },
     -- How the repl window will be displayed
     -- See below for more information
-    repl_open_cmd = require('iron.view').bottom(40),
+    repl_open_cmd = require('iron.view').bottom(0.3),
   },
   -- Iron doesn't setkeymaps by default anymore.
   -- You can set them here or manually add keymaps to the functions in iron.core
