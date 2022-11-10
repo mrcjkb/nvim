e5cf15a1757238c96d17865b3650c2e0995aab23
Marc Jakobi <mrcjkb89@outlook.com>
2022-11-10 08:40:52 +0100

    remove Gradle command

--- lua/commands.lua
+++ lua/commands.lua
@@ -6,9 +6,6 @@ api.nvim_create_user_command('Q', 'bd % <CR>', {})
 -- delete current buffer (force) -- FIXME
 -- api.nvim_create_user_command('Q!', 'bd! % <CR>', {}) 
 
--- Syntactic sugar for gng 'gw' command (TODO: Is this still needed? Move to java-specific config?)
-api.nvim_create_user_command('Gradle', 'split | resize 10 | terminal gw<args>', {nargs='*'})
-
 -- Pandoc shortcut
 api.nvim_create_user_command('Pd', 'split | resize 10 | terminal pandoc % -f markdown-implicit_figures -s -o <args>', {nargs='*'}) 
 
