3bdfc541f5fd53018eab13bb30ebb52790dc0a1a
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-14 16:52:12 +0100

    fix split resize keymaps

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -52,21 +52,22 @@ keymap.set('n', '[L',  ':lfirst<CR>' , {})
 keymap.set('n', ']L',  ':llast<CR>' , {})
 
 -- Resize vertical splits
+local toIntegral = math.ceil
 keymap.set('n', '<leader>+',  function()
   local curWinWidth = api.nvim_win_get_width(0)
-  api.nvim_win_set_width(0, curWinWidth * 3 / 2)
+  api.nvim_win_set_width(0, toIntegral(curWinWidth * 3 / 2))
 end, {silent = true,})
 keymap.set('n', '<leader>-',  function()
   local curWinWidth = api.nvim_win_get_width(0)
-  api.nvim_win_set_width(0, curWinWidth * 2 / 3)
+  api.nvim_win_set_width(0, toIntegral(curWinWidth * 2 / 3))
 end, {silent = true,})
 keymap.set('n', '<space>+',  function()
   local curWinHeight = api.nvim_win_get_height(0)
-  api.nvim_win_set_height(0, curWinHeight * 3 / 2)
+  api.nvim_win_set_height(0, toIntegral(curWinHeight * 3 / 2))
 end, {silent = true,})
 keymap.set('n', '<space>-',  function()
   local curWinHeight = api.nvim_win_get_height(0)
-  api.nvim_win_set_height(0, curWinHeight * 2 / 3)
+  api.nvim_win_set_height(0, toIntegral(curWinHeight * 2 / 3))
 end, {silent = true,})
 
 -- Remap Esc to switch to normal mode and Ctrl-Esc to pass Esc to terminal
