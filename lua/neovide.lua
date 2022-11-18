91dfc6a3195d0f947226560fe62e05126fda9438
Marc Jakobi <marc.jakobi@tiko.energy>
2022-11-18 09:31:23 +0100

    use JetBrains Mono Nerd Font Mono in Neovide

--- lua/neovide.lua
+++ lua/neovide.lua
@@ -3,7 +3,7 @@ if not vim.g.neovide then
 end
 
 local font_size = 14
-vim.o.guifont = 'JetBrains mono' .. ':h' .. font_size
+vim.o.guifont = 'JetBrains Mono Nerd Font Mono' .. ':h' .. font_size
 vim.g.neovide_hide_mouse_when_typing = true
 -- vim.g.neovide_underline_automatic_scaling = true -- Noticeable for font sizes above 15
 
