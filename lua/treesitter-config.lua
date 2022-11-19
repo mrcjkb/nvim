e6adda75bee9cfb2d9d2e2ada627be5510a43b0d
Marc Jakobi <mrcjkb89@outlook.com>
2022-11-19 18:54:46 +0100

    update treesitter-config

--- lua/treesitter-config.lua
+++ lua/treesitter-config.lua
@@ -48,22 +48,22 @@ configs.setup {
       enable = true,
       set_jumps = true, -- whether to set jumps in the jumplist
       goto_next_start = {
-        ["]f"] = "@function.outer",
+        ["]m"] = "@function.outer",
         ["]P"] = "@parameter.outer",
         ["]c"] = "@class.outer",
       },
       goto_next_end = {
-        ["]f"] = "@function.outer",
+        ["]m"] = "@function.outer",
         ["]P"] = "@parameter.outer",
         ["]c"] = "@class.outer",
       },
       goto_previous_start = {
-        ["[f"] = "@function.outer",
+        ["[m"] = "@function.outer",
         ["[P"] = "@parameter.outer",
         ["[c"] = "@class.outer",
       },
       goto_previous_end = {
-        ["[f"] = "@function.outer",
+        ["[m"] = "@function.outer",
         ["[P"] = "@parameter.outer",
         ["[c"] = "@class.outer",
       },
