fa0df760ba007a50eb1ebad284ce7f8ac00eb9a0
Marc Jakobi <mrcjkb89@outlook.com>
2022-11-19 17:20:27 +0100

    cleanup treesitter-config

--- lua/treesitter-config.lua
+++ lua/treesitter-config.lua
@@ -101,7 +101,30 @@ configs.setup {
         goto_previous_usage = "<a-#>",
       },
     },
-  }
+  },
+  playground = { -- Toggle with :TSPlaygroundToggle
+    enable = true,
+    disable = {},
+    updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+    persist_queries = false, -- Whether the query persists across vim sessions
+    keybindings = {
+      toggle_query_editor = 'o',
+      toggle_hl_groups = 'i',
+      toggle_injected_languages = 't',
+      toggle_anonymous_nodes = 'a',
+      toggle_language_display = 'I',
+      focus_language = 'f',
+      unfocus_language = 'F',
+      update = 'R',
+      goto_node = '<cr>',
+      show_help = '?',
+    },
+  },
+  query_linter = {
+    enable = true,
+    use_virtual_text = true,
+    lint_events = {'BufWrite', 'CursorHold'},
+  },
 }
 
 -- Tree-sitter based folding
