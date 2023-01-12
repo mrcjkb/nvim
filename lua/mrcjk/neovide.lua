51a74a7dbd0fe82bebdc88b4864bb00cf998b82c
Marc Jakobi <mrcjkb89@outlook.com>
2023-01-12 09:41:56 +0100

    reduce neovide font size to 12

--- lua/mrcjk/neovide.lua
+++ lua/mrcjk/neovide.lua
@@ -2,7 +2,7 @@ if not vim.g.neovide then
   return
 end
 
-local font_size = 14
+local font_size = 12
 vim.o.guifont = 'JetBrains Mono Nerd Font Mono' .. ':h' .. font_size
 vim.g.neovide_hide_mouse_when_typing = true
 -- vim.g.neovide_underline_automatic_scaling = true -- Noticeable for font sizes above 15
