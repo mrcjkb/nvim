be45fa320b0c8a0876c4121384f6dc06f61e369e
Marc Jakobi <mrcjkb89@outlook.com>
2023-01-16 19:45:00 +0100

    add neovim settings to sumneko_lua

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -232,6 +232,25 @@ require('neodev').setup {
 lspconfig.sumneko_lua.setup {
   on_attach = on_attach,
   capabilities = capabilities,
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = { 'vim' },
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file('', true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
 }
 
 local rust_tools_opts = {
