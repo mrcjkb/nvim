ccf475be92f28ac9d1c369e118a21d45eb271395
Marc Jakobi <mrcjkb89@outlook.com>
2023-01-16 01:37:12 +0100

    Set fidget title highlight

--- lua/mrcjk/highlights.lua
+++ lua/mrcjk/highlights.lua
@@ -1,3 +1,4 @@
 -- Highlight terminal cursor
 vim.api.nvim_set_hl(0, 'TermCursor', { link = 'Cursor' })
 vim.api.nvim_set_hl(0, 'TermCursorNC', { bg = 'red', fg = 'white', ctermbg = 1, ctermfg = 15 })
+vim.api.nvim_set_hl(0, 'FidgetTitle', { link = 'DiagnosticHint' })
