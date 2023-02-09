b97d4a5546c285f4fd2362b5c03466d42c87b4d8
Marc Jakobi <mrcjkb89@outlook.com>
2023-02-09 01:26:15 +0100

    fix

--- nix/nvim_module.nix
+++ nix/nvim_module.nix
@@ -107,7 +107,6 @@ with lib; let
   customRC =
     ''
       local pack_path = vim.fn.stdpath("data") .. "/site/pack/dev",
-      vim.opt.packpath:append(pack_path)
       local dev_plugins_dir = pack_path .. '/opt'
       local dev_plugin_path
     ''
