cf0a462e824b550c31674d131b2c99dffd3c3d6a
Marc Jakobi <mrcjkb89@outlook.com>
2023-03-11 21:57:35 +0100

    fix: replace has_file, which has been removed from neodev

--- nvim/lua/plugin/neodev.lua
+++ nvim/lua/plugin/neodev.lua
@@ -1,7 +1,24 @@
+local function fqn(fname)
+  fname = vim.fn.fnamemodify(fname, ':p')
+  return vim.loop.fs_realpath(fname) or fname
+end
+
+local function exists(fname)
+  local stat = vim.loop.fs_stat(fname)
+  return (stat and stat.type) or false
+end
+
+---@param root_dir string
+---@param file string
+local function has_file(root_dir, file)
+  root_dir = fqn(root_dir)
+  file = fqn(file)
+  return exists(file) and file:find(root_dir, 1, true) == 1
+end
+
 require('neodev').setup {
   override = function(root_dir, library)
-    local util = require('neodev.util')
-    if util.has_file(root_dir, '/etc/nixos') or util.has_file(root_dir, 'nvim-config') then
+    if has_file(root_dir, '/etc/nixos') or has_file(root_dir, 'nvim-config') then
       library.enabled = true
       library.plugins = true
     end
