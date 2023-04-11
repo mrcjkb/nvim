97147c29a535c957a49d7f68232c80bad20ad034
Marc Jakobi <marc.jakobi@tiko.energy>
2023-04-11 22:21:11 +0200

    fix treehopper config

--- nvim/lua/plugin/treehopper.lua
+++ nvim/lua/plugin/treehopper.lua
@@ -1,4 +1,4 @@
--- local tsht = require('tsht')
+local tsht = require('tsht')
 
--- vim.keymap.set('x', 'm', tsht.nodes { noremap = true, silent = true })
--- vim.keymap.set('o', 'm', tsht.nodes { noremap = false, silent = true })
+vim.keymap.set('x', 'm', tsht.nodes, { noremap = true, silent = true })
+vim.keymap.set('o', 'm', tsht.nodes, { noremap = false, silent = true })
