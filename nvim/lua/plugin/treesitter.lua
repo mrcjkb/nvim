bb85242766b0de4337032d987c200e65f270fc61
Marc Jakobi <marc.jakobi@tiko.energy>
2023-04-17 20:48:14 +0200

    treesitter fix?

--- nvim/lua/plugin/treesitter.lua
+++ nvim/lua/plugin/treesitter.lua
@@ -1,8 +1,7 @@
 local configs = require('nvim-treesitter.configs')
 configs.setup {
-  -- ensure_installed = 'all',
-  -- ignore_install = { 'gitignore' }, -- broken
-  -- auto_install = true, -- Automatically install missing parsers when entering buffer
+  ensure_installed = 'all',
+  auto_install = false, -- Do not automatically install missing parsers when entering buffer
   highlight = {
     enable = true,
     disable = function(_, buf)
