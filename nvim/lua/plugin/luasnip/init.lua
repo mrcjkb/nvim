616ca28a723c606ca6039dc849dbfee55c31d86d
Marc Jakobi <marc.jakobi@tiko.energy>
2023-05-25 09:01:46 +0200

    add some choices to lang snippet

--- nvim/lua/plugin/luasnip/init.lua
+++ nvim/lua/plugin/luasnip/init.lua
@@ -106,7 +106,21 @@ local language_pragma = s({
   dscr = 'LANGUAGE pragma',
 }, {
   text('{-# LANGUAGE '),
-  insert(1),
+  choice(1, {
+    insert(1),
+    text('ScopedTypeVariables'),
+    text('RecordWildCards'),
+    text('LamdaCase'),
+    text('QuasiQuotes'),
+    text('ViewPatterns'),
+    text('DerivingVia'),
+    text('DeriveAnyClass'),
+    text('DeriveGeneric'),
+    text('MultiParamTypeClasses'),
+    text('TypeFamilies'),
+    text('DataKinds'),
+    text('OverloadedLists'),
+  }),
   text(' #-}'),
 })
 table.insert(haskell_snippets, language_pragma)
