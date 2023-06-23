c1b4dc3dd17a620c9c4d5874a8dba73cf0ac2947
Marc Jakobi <marc.jakobi@tiko.energy>
2023-06-23 16:39:01 +0200

    correct unit

--- nvim/lua/plugin/treesitter.lua
+++ nvim/lua/plugin/treesitter.lua
@@ -5,7 +5,7 @@ configs.setup {
   highlight = {
     enable = true,
     disable = function(_, buf)
-      local max_filesize = 100 * 1024 -- 100 KB
+      local max_filesize = 100 * 1024 -- 100 kB
       local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
       if ok and stats and stats.size > max_filesize then
         return true
