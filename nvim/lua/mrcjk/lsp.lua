a938780f4a39a190a0df0d2dee088e772b2ae165
Marc Jakobi <marc.jakobi@tiko.energy>
2023-06-25 22:16:46 +0200

    fix cmp_lsp check

--- nvim/lua/mrcjk/lsp.lua
+++ nvim/lua/mrcjk/lsp.lua
@@ -171,8 +171,8 @@ function lsp.on_dap_attach(bufnr)
 end
 
 -- Set up lspconfig.
-local cmp_lsp = require('cmp_nvim_lsp')
-local capabilities = cmp_lsp and cmp_lsp.default_capabilities() or vim.lsp.protocol.make_client_capabilities()
+local has_cmp_lsp, cmp_lsp = pcall(require, 'cmp_nvim_lsp')
+local capabilities = has_cmp_lsp and cmp_lsp.default_capabilities() or vim.lsp.protocol.make_client_capabilities()
 capabilities = require('lsp-selection-range').update_capabilities(capabilities)
 -- Enable preliminary support for workspace/didChangeWatchedFiles
 vim.tbl_deep_extend('keep', capabilities, {
