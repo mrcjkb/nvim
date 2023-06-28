294a6191f85206ca89885ab53c2b16d2a7e4eace
Marc Jakobi <marc.jakobi@tiko.energy>
2023-06-28 16:42:51 +0200

    add to-upstream nix injections

--- nvim/after/queries/nix/injections.scm
+++ nvim/after/queries/nix/injections.scm
@@ -6,9 +6,40 @@
     )
   )
   argument: [
-    (string_expression (string_fragment) @injection.content)
-    (indented_string_expression (string_fragment) @injection.content)
-  ]
-  (#set! injection.language "nix")
+    (string_expression (string_fragment) @injection.content (#set! injection.language "nix"))
+    (indented_string_expression (string_fragment) @injection.content (#set! injection.language "nix"))
+  ] (#set! injection.combined))
+
+
+;; The below queries will be upstreamed
+
+;; (nixosTest) testScript
+((binding
+  ((attrpath) @_attr_name) 
+    (#eq? _attr_name "nodes")
   )
-  @combined
+  (binding
+   ((attrpath) @_func_name) (#eq? @_func_name "testScript")
+   (_
+     (string_fragment) @injection.content (#set! injection.language "python")
+   ))
+   (#set! injection.combined))
+
+;; home-manager Neovim plugin config
+(attrset_expression
+  (binding_set
+    (binding
+      (attrpath) @_ty_attr
+      (_
+        (string_fragment) @_ty
+      )
+      (#eq? @_ty_attr "type")
+      (#eq? @_ty "lua"))
+    (binding
+      (attrpath) @_cfg_attr
+      (_
+        (string_fragment) @injection.content (#set! injection.language "lua")
+      )
+      (#eq? @_cfg_attr "config")
+    ))
+    (#set! injection.combined))
