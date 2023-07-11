85500ac400132e17ebf9c0874b981d959aaa45b4
Marc Jakobi <marc.jakobi@tiko.energy>
2023-07-11 23:42:44 +0200

    add autocommands to toggle relativenumber

--- nvim/lua/mrcjk/autocommands.lua
+++ nvim/lua/mrcjk/autocommands.lua
@@ -9,3 +9,26 @@ api.nvim_create_autocmd('BufWritePre', {
     vim.cmd.setlocal('noundofile')
   end,
 })
+
+local numbertoggle = api.nvim_create_augroup('numbertoggle', { clear = true })
+-- Toggle between relative/absolute line numbers
+api.nvim_create_autocmd({ 'BufEnter', 'FocusGained', 'InsertLeave', 'CmdlineLeave', 'WinEnter' }, {
+  pattern = '*',
+  group = numbertoggle,
+  callback = function()
+    if vim.o.nu and vim.api.nvim_get_mode().mode ~= 'i' then
+      vim.opt.relativenumber = true
+    end
+  end,
+})
+
+api.nvim_create_autocmd({ 'BufLeave', 'FocusLost', 'InsertEnter', 'CmdlineEnter', 'WinLeave' }, {
+  pattern = '*',
+  group = numbertoggle,
+  callback = function()
+    if vim.o.nu then
+      vim.opt.relativenumber = false
+      vim.cmd.redraw()
+    end
+  end,
+})
