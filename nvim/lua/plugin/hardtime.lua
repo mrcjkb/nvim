9960320d4864cb3af014ba5445927b78e8660cae
Marc Jakobi <marc.jakobi@tiko.energy>
2023-07-12 21:17:56 +0200

    hardtime-nvim: manually set restricted_keys

--- nvim/lua/plugin/hardtime.lua
+++ nvim/lua/plugin/hardtime.lua
@@ -5,6 +5,17 @@ require('hardtime').setup {
   hint = true,
   notification = true,
   allow_different_key = false,
+  restricted_keys = {
+    ['h'] = { 'n', 'x' },
+    ['j'] = { 'n', 'x' },
+    ['k'] = { 'n', 'x' },
+    ['l'] = { 'n', 'x' },
+    ['gj'] = { 'n', 'x' },
+    ['gk'] = { 'n', 'x' },
+    ['<CR>'] = { 'n', 'x' },
+    ['<C-M>'] = { 'n', 'x' },
+    ['<C-N>'] = { 'n', 'x' },
+  },
   disabled_filetypes = {
     'qf',
     'netrw',
