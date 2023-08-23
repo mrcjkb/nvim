e05e1b9038b16064ccdc87fa3a177667f1c0b76f
Marc Jakobi <marc.jakobi@tiko.energy>
2023-08-23 23:49:12 +0200

    disable yanky (test)

--- nvim/lua/plugin/yanky.lua
+++ nvim/lua/plugin/yanky.lua
@@ -1,13 +1,13 @@
-require('yanky').setup {
-  highlight = {
-    on_put = true,
-    on_yank = true,
-    timer = 500,
-  },
-  preserve_cursor_position = {
-    enabled = true,
-  },
-}
+-- require('yanky').setup {
+--   highlight = {
+--     on_put = true,
+--     on_yank = true,
+--     timer = 500,
+--   },
+--   preserve_cursor_position = {
+--     enabled = true,
+--   },
+-- }
 vim.keymap.set({ 'n', 'x' }, 'p', '<Plug>(YankyPutAfter)')
 vim.keymap.set({ 'n', 'x' }, 'P', '<Plug>(YankyPutBefore)')
 vim.keymap.set({ 'n', 'x' }, 'gp', '<Plug>(YankyGPutAfter)')
