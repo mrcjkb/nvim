d6cb97053ccf04187eb6d1a1adc10a08a3236819
Marc Jakobi <marc.jakobi@tiko.energy>
2023-09-02 02:08:59 +0200

    lazy-init neotest

--- nvim/lua/plugin/neotest.lua
+++ nvim/lua/plugin/neotest.lua
@@ -1,40 +1,50 @@
-local neotest = require('neotest')
-neotest.setup {
-  adapters = {
-    require('neotest-haskell') {
-      frameworks = {
-        {
-          framework = 'tasty',
-          modules = { 'Test.Tasty', 'T', 'Test.Util', 'TestUtil' },
+local keymap_opts = { noremap = true, silent = true }
+
+---Neotest is quite heavy on startup, so this initializes it lazily, on keymap
+local function init(fun)
+  local neotest = require('neotest')
+  neotest.setup {
+    adapters = {
+      require('neotest-haskell') {
+        frameworks = {
+          {
+            framework = 'tasty',
+            modules = { 'Test.Tasty', 'T', 'Test.Util', 'TestUtil' },
+          },
+          'hspec',
+          'sydtest',
         },
-        'hspec',
-        'sydtest',
       },
+      require('neotest-rust'),
     },
-    require('neotest-rust'),
-  },
-  icons = {
-    failed = '',
-    passed = '',
-    running = '',
-    skipped = '',
-    unknown = '',
-  },
-  quickfix = {
-    enabled = false,
-    open = false,
-  },
-}
-local opts = { noremap = true }
+    icons = {
+      failed = '',
+      passed = '',
+      running = '',
+      skipped = '',
+      unknown = '',
+    },
+    quickfix = {
+      enabled = false,
+      open = false,
+    },
+  }
+  vim.keymap.del('n', '<leader>nr', keymap_opts)
+  vim.keymap.set('n', '<leader>nr', neotest.run.run, keymap_opts)
+  vim.keymap.del('n', '<leader>nf', keymap_opts)
+  vim.keymap.set('n', '<leader>nf', function()
+    neotest.run.run(vim.api.nvim_buf_get_name(0))
+  end, keymap_opts)
+  vim.keymap.set('n', '<leader>no', neotest.output.open, keymap_opts)
+  vim.keymap.set('n', '<leader>ns', neotest.summary.toggle, keymap_opts)
+  fun()
+end
+
 vim.keymap.set('n', '<leader>nr', function()
-  neotest.run.run()
-end, opts)
+  init(require('neotest').run.run)
+end, keymap_opts)
 vim.keymap.set('n', '<leader>nf', function()
-  neotest.run.run(vim.api.nvim_buf_get_name(0))
-end, opts)
-vim.keymap.set('n', '<leader>no', function()
-  neotest.output.open()
-end, opts)
-vim.keymap.set('n', '<leader>ns', function()
-  neotest.summary.toggle()
-end, opts)
+  init(function()
+    require('neotest').run.run(vim.api.nvim_buf_get_name(0))
+  end)
+end, keymap_opts)
