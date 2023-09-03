635cdadc254f3ce14c99ae642296b1f75d6b1f32
Marc Jakobi <marc.jakobi@tiko.energy>
2023-09-03 22:23:15 +0200

    add keymaps for haskell package files

--- nvim/lua/lang/haskell.lua
+++ nvim/lua/lang/haskell.lua
@@ -8,7 +8,8 @@ end
 vim.keymap.set('n', '<space>gp', ht.project.telescope_package_grep, keymap_opts)
 vim.keymap.set('n', '<space>gf', ht.project.telescope_package_files, keymap_opts)
 vim.keymap.set('n', '<space>gp', ht.project.telescope_package_grep, keymap_opts)
-vim.keymap.set('n', '<space>gf', ht.project.telescope_package_files, keymap_opts)
+vim.keymap.set('n', '<space>gy', ht.project.open_package_yaml, keymap_opts)
+vim.keymap.set('n', '<space>gc', ht.project.open_package_cabal, keymap_opts)
 
 if ht.dap then
   local bufnr = vim.api.nvim_get_current_buf()
