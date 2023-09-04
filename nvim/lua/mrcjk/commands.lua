c237520ef3337964f0a74d11ba50119ca0824160
Marc Jakobi <marc.jakobi@tiko.energy>
2023-09-04 20:55:56 +0200

    add W command -> :w

--- nvim/lua/mrcjk/commands.lua
+++ nvim/lua/mrcjk/commands.lua
@@ -3,6 +3,7 @@ local api = vim.api
 api.nvim_create_user_command('DeleteOtherBufs', '%bd|e#', {})
 -- delete current buffer
 api.nvim_create_user_command('Q', 'bd % <CR>', {})
+api.nvim_create_user_command('W', 'w<CR>', {})
 -- delete current buffer (force) -- FIXME
 -- api.nvim_create_user_command('Q!', 'bd! % <CR>', {})
 
