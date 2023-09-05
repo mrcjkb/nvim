987f150acd347160bd6b565b25ee3cf066f6bd72
Marc Jakobi <marc.jakobi@tiko.energy>
2023-09-05 22:33:48 +0200

    add some nvim-surround surrounds for haskell

--- nvim/ftplugin/haskell.lua
+++ nvim/ftplugin/haskell.lua
@@ -14,3 +14,44 @@ vim.keymap.set('n', '<leader>rt', ht.repl.paste_type, keymap_opts)
 vim.keymap.set('n', '<leader>rw', ht.repl.cword_type, keymap_opts)
 -- TODO: remove when ambiguous target issue is resolved
 vim.keymap.set('n', '<leader>tt', '<cmd>TermExec cmd="cabal v2-repl %"<CR>', keymap_opts)
+
+-- nvim-surround
+
+---@param items string[]
+---@param prompt string?
+---@param label_fn (fun(item:string):string) | nil
+---@return string|nil
+local function ui_select_sync(items, prompt, label_fn)
+  label_fn = label_fn or function(str)
+    return str
+  end
+  local choices = { prompt }
+  for i, item in ipairs(items) do
+    table.insert(choices, string.format('%d: %s', i, label_fn(item)))
+  end
+  local choice = vim.fn.inputlist(choices)
+  if choice < 1 or choice > #items then
+    return nil
+  end
+  return items[choice]
+end
+
+---@diagnostic disable-next-line: missing-fields
+require('nvim-surround').buffer_setup {
+  surrounds = {
+    ---@diagnostic disable-next-line: missing-fields
+    ['L'] = {
+      add = function()
+        local level = ui_select_sync({ 'e', 'w', 'i', 'd', 't' }, 'Select a log level')
+        return level and { {
+          '[Log.' .. level .. '|',
+        }, { '|]' } } or {}
+      end,
+      -- TODO: Use tree-sitter query (see ["f"]) in nvim-surround
+    },
+    ---@diagnostic disable-next-line: missing-fields
+    ['Q'] = {
+      add = { '[qq|', '|]' },
+    },
+  },
+}
