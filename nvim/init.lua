c621f9ce72904cfba68989afe16ca9717bb4dd50
Marc Jakobi <marc.jakobi@tiko.energy>
2023-09-13 15:42:42 +0200

    move native-plugin configs to init.lua + disable some builtin ones

--- nvim/init.lua
+++ nvim/init.lua
@@ -123,6 +123,33 @@ g.editorconfig = true
 
 vim.opt.colorcolumn = '100'
 
+-- Native plugins
+cmd.filetype('plugin', 'indent', 'on')
+cmd.packadd('cfilter')
+
+-- Disable builtin plugins
+g.loaded_gzip = 1
+g.loaded_zip = 1
+g.loaded_zipPlugin = 1
+g.loaded_tar = 1
+g.loaded_tarPlugin = 1
+
+g.loaded_getscript = 1
+g.loaded_getscriptPlugin = 1
+g.loaded_vimball = 1
+g.loaded_vimballPlugin = 1
+g.loaded_2html_plugin = 1
+
+g.loaded_matchit = 1
+g.loaded_matchparen = 1
+g.loaded_logiPat = 1
+g.loaded_rrhelper = 1
+
+g.loaded_netrw = 1
+g.loaded_netrwPlugin = 1
+g.loaded_netrwSettings = 1
+g.loaded_netrwFileHandlers = 1
+
 -- Plugin settings
 local keymap_opts = { noremap = true, silent = true }
 local telescope = require('telescope')
--- nvim/plugin/native-plugins.lua
+++
@@ -1,5 +0,0 @@
-local cmd = vim.cmd
-
-cmd('filetype plugin indent on')
-cmd('packadd cfilter')
-vim.g.loaded_matchit = 1
