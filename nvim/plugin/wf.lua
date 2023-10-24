c738d33a3eb6c507983288a7e87035b7bbccb687
Marc Jakobi <marc.jakobi@tiko.energy>
2023-10-24 20:37:24 +0200

    remove some annoying which-key maps

--- nvim/plugin/wf.lua
+++ nvim/plugin/wf.lua
@@ -3,25 +3,6 @@ local wf = require('wf')
 wf.setup()
 
 local which_key = require('wf.builtin.which_key')
-local register = require('wf.builtin.register')
-local buffer = require('wf.builtin.buffer')
-local mark = require('wf.builtin.mark')
-
--- Register
-vim.keymap.set(
-  'n',
-  '<leader>wr',
-  -- register(opts?: table) -> function
-  -- opts?: option
-  register(),
-  { noremap = true, silent = true, desc = '[wf.nvim] register' }
-)
-
--- Buffer
-vim.keymap.set('n', '<Space>wb', buffer(), { noremap = true, silent = true, desc = '[wf.nvim] buffer' })
-
--- Mark
-vim.keymap.set('n', "'", mark(), { nowait = true, noremap = true, silent = true, desc = '[wf.nvim] mark' })
 
 -- Which Key
 vim.keymap.set(
@@ -29,33 +10,4 @@ vim.keymap.set(
   '<leader>ww',
   which_key { text_insert_in_advance = '' },
   { noremap = true, silent = true, desc = '[wf.nvim] which-key' }
-)
-
--- Which Key
-vim.keymap.set(
-  'n',
-  '<leader>',
-  which_key { text_insert_in_advance = '<Leader>' },
-  { noremap = true, silent = true, desc = '[wf.nvim] which-key /' }
-)
-
-vim.keymap.set(
-  'n',
-  '<space>',
-  which_key { text_insert_in_advance = '<Space>' },
-  { noremap = true, silent = true, desc = '[wf.nvim] which-key /' }
-)
-
-vim.keymap.set(
-  'n',
-  '[',
-  which_key { text_insert_in_advance = '[' },
-  { noremap = true, silent = true, desc = '[wf.nvim] which-key [' }
-)
-
-vim.keymap.set(
-  'n',
-  ']',
-  which_key { text_insert_in_advance = ']' },
-  { noremap = true, silent = true, desc = '[wf.nvim] which-key ]' }
 )
