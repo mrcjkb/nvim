09bfbad914ea302c69ed32b5aa648d6a78d677a3
Marc Jakobi <marc.jakobi@tiko.energy>
2023-10-24 21:03:16 +0200

    tree-sitter: add list of files to disable highlighting for

--- nvim/plugin/treesitter.lua
+++ nvim/plugin/treesitter.lua
@@ -1,3 +1,10 @@
+-- Files that have trouble with syntax highlighting
+local disabled_files = {
+  'Enums.hs',
+  'all-packages.nix',
+  'hackage-packages.nix',
+}
+
 ---@diagnostic disable: missing-fields
 local configs = require('nvim-treesitter.configs')
 configs.setup {
@@ -6,8 +13,13 @@ configs.setup {
   highlight = {
     enable = true,
     disable = function(_, buf)
+      local fname = vim.api.nvim_buf_get_name(buf)
+      local short_name = vim.fn.fnamemodify(fname, ':t')
+      if vim.tbl_contains(disabled_files, short_name) then
+        return true
+      end
       local max_filesize = 100 * 1024 -- 100 KiB
-      local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+      local ok, stats = pcall(vim.loop.fs_stat, fname)
       if ok and stats and stats.size > max_filesize then
         return true
       end
