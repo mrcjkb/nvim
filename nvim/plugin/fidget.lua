29daccccf6b37e82f01b847c8e7248e8815dce75
Marc Jakobi <marc.jakobi@tiko.energy>
2023-11-28 08:57:15 +0100

    some fidget.nvim configuration

---
+++ nvim/plugin/fidget.lua
@@ -0,0 +1,11 @@
+require('fidget').setup {
+  -- Options related to LSP progress subsystem
+  progress = {
+    ignore_done_already = true, -- Ignore new tasks that are already complete
+
+    -- Options related to how LSP progress messages are displayed as notifications
+    display = {
+      render_limit = 2, -- How many LSP messages to show at once
+    },
+  },
+}
