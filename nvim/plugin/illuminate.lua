7a60b1efa2d7c656584410d9a5ee10d58b850875
Marc Jakobi <marc.jakobi@tiko.energy>
2023-11-30 09:17:12 +0100

    fix illuminate

--- nvim/plugin/illuminate.lua
+++ nvim/plugin/illuminate.lua
@@ -2,7 +2,9 @@ local illuminate = require('illuminate')
 local files = require('mrcjk.files')
 illuminate.configure {
   delay = 200,
-  should_enable = files.disable_treesitter_features,
+  should_enable = function(bufnr)
+    return not files.disable_treesitter_features(bufnr)
+  end,
 }
 vim.keymap.set('n', ']]', function()
   illuminate.goto_next_reference(true)
