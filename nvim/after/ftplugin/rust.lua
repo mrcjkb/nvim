3c8ae88d1f2c522d24cdb396c8c2cae9f77895fe
Marc Jakobi <marc.jakobi@tiko.energy>
2024-01-20 18:20:35 +0100

    add rust keymaps

---
+++ nvim/after/ftplugin/rust.lua
@@ -0,0 +1,42 @@
+local bufnr = vim.api.nvim_get_current_buf()
+
+local function desc(description)
+  return { noremap = true, silent = true, buffer = bufnr, desc = description }
+end
+
+vim.keymap.set('n', '<space>rd', function()
+  vim.cmd.RustLsp('debuggables')
+end, desc('rust: open Cargo.toml'))
+vim.keymap.set('n', '<space>rr', function()
+  vim.cmd.RustLsp('runnables')
+end, desc('rust: open Cargo.toml'))
+vim.keymap.set('n', '<space>rme', function()
+  vim.cmd.RustLsp('expandMacro')
+end, desc('rust: open Cargo.toml'))
+vim.keymap.set('n', '<C-k>', function()
+  vim.cmd.RustLsp { 'moveItem', 'up' }
+end, desc('rust: move item up'))
+vim.keymap.set('n', '<C-j>', function()
+  vim.cmd.RustLsp { 'moveItem', 'down' }
+end, desc('rust: move item down'))
+vim.keymap.set('v', 'K', function()
+  vim.cmd.RustLsp { 'hover', 'range' }
+end, desc('rust: hover range'))
+vim.keymap.set('n', '<space>re', function()
+  vim.cmd.RustLsp('explainError')
+end, desc('rust: explain error'))
+vim.keymap.set('n', '<space>rd', function()
+  vim.cmd.RustLsp('renderDiagnostic')
+end, desc('rust: render diagnostic'))
+vim.keymap.set('n', '<space>gc', function()
+  vim.cmd.RustLsp('openCargo')
+end, desc('rust: open Cargo.toml'))
+vim.keymap.set('n', '<space>gp', function()
+  vim.cmd.RustLsp('parentModule')
+end, desc('rust: open Cargo.toml'))
+vim.keymap.set('n', 'J', function()
+  vim.cmd.RustLsp('joinLines')
+end, desc('rust: join lines'))
+vim.keymap.set('n', '<space>rs', function()
+  vim.cmd.RustLsp('ssr')
+end, desc('rust: join lines'))
