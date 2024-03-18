9898d55cf551fdf4214e656329f99959201646db
Marc Jakobi <marc.jakobi@tiko.energy>
2024-03-18 10:59:15 +0100

    add lua injection

--- nvim/queries/lua/injections.scm
+++ nvim/queries/lua/injections.scm
@@ -1,1 +1,11 @@
+;; extends
+
 ; ((string ("string_content") @query) (#lua-match? @query "^%s*;;+%s?"))
+
+;; TODO:
+; ((comment 
+;     content: (comment_content) @injection.language)
+; . (expression_list
+;         value: (string content: (string_content) @injection.content))
+;   (#gsub! @injection.language "%-%-[[%s*([%w%p]+)%s*%]%]" "%1")
+; )
