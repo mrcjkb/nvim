7f3dde8d0b2b896410458d1318fd8340b4e1c260
Marc Jakobi <marc.jakobi@tiko.energy>
2024-03-21 08:55:26 +0100

    highlight-colors: disable by default and add keymap to toggle

--- nvim/plugin/highlight-colors.lua
+++ nvim/plugin/highlight-colors.lua
@@ -1,3 +1,8 @@
 require('nvim-highlight-colors').setup {
   -- render = 'virtual',
 }
+vim.cmd.HighlightColors('off')
+
+vim.keymap.set('n', '<leader>ct', function()
+  vim.cmd.HighlightColors('toggle')
+end, { noremap = true, silent = true, desc = 'highlight [c]olors: [t]oggle' })
