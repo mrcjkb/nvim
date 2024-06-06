13415ef8be91b47f38820ce679bbaab71b9f71f8
Marc Jakobi <marc.jakobi@tiko.energy>
2024-06-06 16:19:57 +0200

    which-key: remove patch

--- nix/plugin-overlay.nix
+++ nix/plugin-overlay.nix
@@ -91,8 +91,6 @@ in {
     hardtime-nvim = mkNvimPlugin inputs.hardtime-nvim "hardtime.nvim";
     term-edit-nvim = mkNvimPlugin inputs.term-edit-nvim "term-edit.nvim";
     other-nvim = mkNvimPlugin inputs.other-nvim "other.nvim";
-    which-key-nvim = (mkNvimPlugin inputs.which-key-nvim "which-key.nvim").overrideAttrs (oa: {
-      patches = [./which-key.patch];
-    });
+    which-key-nvim = mkNvimPlugin inputs.which-key-nvim "which-key.nvim";
   };
 }
