95faba14e0ec162f2dfbd7fd5f350298490370d3
Marc Jakobi <marc.jakobi@tiko.energy>
2024-08-20 10:04:44 +0200

    add substitute keymap descriptions

--- nvim/plugin/substitute.lua
+++ nvim/plugin/substitute.lua
@@ -3,7 +3,7 @@ substitute.setup {
   on_substitute = require('yanky.integration').substitute(),
 }
 
-vim.keymap.set('n', '<leader><leader>s', substitute.operator, { noremap = true })
-vim.keymap.set('n', '<leader><leader>ss', substitute.line, { noremap = true })
-vim.keymap.set('n', '<leader><leader>S', substitute.eol, { noremap = true })
-vim.keymap.set('x', '<leader><leader>s', substitute.visual, { noremap = true })
+vim.keymap.set('n', '<leader><leader>s', substitute.operator, { noremap = true, desc = 'Substitute (operator)' })
+vim.keymap.set('n', '<leader><leader>ss', substitute.line, { noremap = true, desc = 'Substitute (line)' })
+vim.keymap.set('n', '<leader><leader>S', substitute.eol, { noremap = true, desc = 'Substitute (eol)' })
+vim.keymap.set('x', '<leader><leader>s', substitute.visual, { noremap = true, desc = 'Substitute (visual)' })
