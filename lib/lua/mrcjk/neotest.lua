3bd05a3065599025312113ebbaa1eaf2667674fa
Marc Jakobi <marc.jakobi@tiko.energy>
2024-09-25 10:28:35 +0200

    neotest: add keymap for neotest output-panel

--- lib/lua/mrcjk/neotest.lua
+++ lib/lua/mrcjk/neotest.lua
@@ -54,6 +54,7 @@ vim.keymap.set('n', '<leader>nw', function()
   neotest.watch.toggle(vim.api.nvim_buf_get_name(0))
 end, desc('[n]eotest: [w]atch file'))
 vim.keymap.set('n', '<leader>no', neotest.output.open, desc('[n]eotest: [o]pen output'))
+vim.keymap.set('n', '<leader>np', neotest.output_panel.toggle, desc('[n]eotest: output-[p]anel'))
 vim.keymap.set('n', '<leader>ns', neotest.summary.toggle, desc('[n]eotest: toggle [s]ummary'))
 
 return true
