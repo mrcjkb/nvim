312951f1c90005db9a2f1f16582516e02779f435
Marc Jakobi <marc.jakobi@tiko.energy>
2024-10-07 19:41:59 +0200

    update nvim-ts-context-commentstring

--- flake.nix
+++ flake.nix
@@ -193,9 +193,7 @@
       flake = false;
     };
     nvim-ts-context-commentstring = {
-      # url = "github:JoosepAlviste/nvim-ts-context-commentstring";
-      url = "github:mrcjkb/nvim-ts-context-commentstring/haskell";
-      # url = "/home/mrcjk/git/github/forks/nvim/nvim-ts-context-commentstring/";
+      url = "github:JoosepAlviste/nvim-ts-context-commentstring";
       flake = false;
     };
     wildfire-nvim = {
