1d0d700562d3e75e69b235dd4444a5dcd9cc2c59
Marc Jakobi <marc.jakobi@tiko.energy>
2025-01-24 22:06:59 +0100

    defer illuminate

--- nvim/plugin/illuminate.lua
+++ nvim/plugin/illuminate.lua
@@ -1,14 +1,27 @@
-local illuminate = require('illuminate')
-local files = require('mrcjk.files')
-illuminate.configure {
-  delay = 200,
-  should_enable = function(bufnr)
-    return not files.disable_treesitter_features(bufnr)
+if vim.g.illuminate_nvim_did_setup then
+  return
+end
+vim.api.nvim_create_autocmd('FileType', {
+  once = true,
+  pattern = '*',
+  group = vim.api.nvim_create_augroup('illuminate-nvim-setup', {}),
+  callback = function()
+    if vim.g.illuminate_nvim_did_setup then
+      return
+    end
+    local illuminate = require('illuminate')
+    local files = require('mrcjk.files')
+    illuminate.configure {
+      delay = 200,
+      should_enable = function(bufnr)
+        return not files.disable_treesitter_features(bufnr)
+      end,
+    }
+    vim.keymap.set('n', ']]', function()
+      illuminate.goto_next_reference(true)
+    end, { noremap = true, silent = true, desc = 'next reference' })
+    vim.keymap.set('n', '[[', function()
+      illuminate.goto_prev_reference(true)
+    end, { noremap = true, silent = true, desc = 'previous reference' })
   end,
-}
-vim.keymap.set('n', ']]', function()
-  illuminate.goto_next_reference(true)
-end, { noremap = true, silent = true, desc = 'next reference' })
-vim.keymap.set('n', '[[', function()
-  illuminate.goto_prev_reference(true)
-end, { noremap = true, silent = true, desc = 'previous reference' })
+})
