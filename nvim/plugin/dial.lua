954bb4ee81dfe4378847b775a2cb3e2e93411ec9
Marc Jakobi <marc.jakobi@tiko.energy>
2025-03-21 15:18:52 +0100

    fix dial.nvim keymaps

--- nvim/plugin/dial.lua
+++ nvim/plugin/dial.lua
@@ -3,8 +3,8 @@ require('lz.n').load {
   ---@type lz.n.KeysSpec[]
   keys = {
     { '<C-a>', '<Plug>(dial-increment)', mode = { 'n', 'v' } },
-    { '<C-x>', '<Plug>(dial-decrement', mode = { 'n', 'v' } },
-    { 'g<C-a>', 'g<Plug>(dial-increment', mode = { 'n', 'v' } },
-    { 'g<C-x>', 'g<Plug>(dial-decrement', mode = { 'n', 'v' } },
+    { '<C-x>', '<Plug>(dial-decrement)', mode = { 'n', 'v' } },
+    { 'g<C-a>', 'g<Plug>(dial-increment)', mode = { 'n', 'v' } },
+    { 'g<C-x>', 'g<Plug>(dial-decrement)', mode = { 'n', 'v' } },
   },
 }
