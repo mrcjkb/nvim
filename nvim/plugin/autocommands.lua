bb3fa8e929114308bef9300b153064c024d3e244
Marc Jakobi <marc.jakobi@tiko.energy>
2025-05-05 09:54:38 +0200

    highlight trailing whitespace

--- nvim/plugin/autocommands.lua
+++ nvim/plugin/autocommands.lua
@@ -330,3 +330,39 @@ api.nvim_create_autocmd({ 'VimEnter', 'FocusGained', 'BufEnter' }, {
   group = api.nvim_create_augroup('ReloadFileOnChange', {}),
   command = 'checktime',
 })
+
+local function should_highlight_extra_whitespace()
+  local ignored_filetypes = {
+    'TelescopePrompt',
+    'help',
+    'dashboard',
+  }
+  if vim.bo.buftype == 'nofile' or vim.bo.buftype == 'terminal' then
+    return false
+  end
+  if vim.tbl_contains(ignored_filetypes, vim.bo.filetype) then
+    return false
+  end
+  local bufnr = vim.api.nvim_get_current_buf()
+  require('editorconfig')
+  if vim.b[bufnr].editorconfig.trim_trailing_whitespace == 'true' then
+    return false
+  end
+  return true
+end
+
+api.nvim_create_autocmd({ 'UIEnter' }, {
+  group = api.nvim_create_augroup('HighlightExtraWhiteSpace', {}),
+  callback = function()
+    local extra_whitespace_hi = 'DiffDelete'
+    if not vim.fn.hlexists(extra_whitespace_hi) then
+      vim.notify_once(string.format('highlight %s does not exist', extra_whitespace_hi), vim.log.levels.ERROR)
+      return
+    end
+    if should_highlight_extra_whitespace() then
+      vim.cmd.match { extra_whitespace_hi, [[/\s\+$/]] }
+    else
+      vim.cmd.match()
+    end
+  end,
+})
