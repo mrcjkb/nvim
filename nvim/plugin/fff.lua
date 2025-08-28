a74b4e0662d994d64e4baa6eeab1cee4f7f96854
Marc Jakobi <marc.jakobi@tiko.energy>
2025-08-28 14:28:47 +0200

    replace telescope find_files with fff

--- nvim/plugin/fff.lua
+++ nvim/plugin/fff.lua
@@ -1,1 +1,2 @@
 vim.keymap.set('n', '<leader>ff', vim.cmd.FFFFind, { noremap = true, silent = true, desc = '[f]ff [f]ind' })
+vim.keymap.set('n', '<leader>tg', vim.cmd.FFFFind, { noremap = true, silent = true, desc = '[f]ff [f]ind' })
