8e01eee83d79ab621bcb9d9c324e7423be7c9478
Marc Jakobi <marc.jakobi@tiko.energy>
2025-09-01 17:20:53 +0200

    tweak clangd config

--- lib/lua/lang/clangd.lua
+++ lib/lua/lang/clangd.lua
@@ -1,5 +1,65 @@
 local M = {}
 
+---@brief
+---
+--- https://clangd.llvm.org/installation.html
+---
+---   ```
+---   ln -s /path/to/myproject/build/compile_commands.json /path/to/myproject/
+---   ```
+--- - clangd relies on a [JSON compilation database](https://clang.llvm.org/docs/JSONCompilationDatabase.html)
+---   specified as compile_commands.json, see https://clangd.llvm.org/installation#compile_commandsjson
+
+-- https://clangd.llvm.org/extensions.html#switch-between-sourceheader
+local function switch_source_header(bufnr, client)
+  local method_name = 'textDocument/switchSourceHeader'
+  ---@diagnostic disable-next-line:param-type-mismatch
+  if not client or not client:supports_method(method_name) then
+    return vim.notify(('method %s is not supported by any servers active on the current buffer'):format(method_name))
+  end
+  local params = vim.lsp.util.make_text_document_params(bufnr)
+  ---@diagnostic disable-next-line:param-type-mismatch
+  client:request(method_name, params, function(err, result)
+    if err then
+      error(tostring(err))
+    end
+    if not result then
+      vim.notify('corresponding file cannot be determined')
+      return
+    end
+    vim.cmd.edit(vim.uri_to_fname(result))
+  end, bufnr)
+end
+
+local function symbol_info(bufnr, client)
+  local method_name = 'textDocument/symbolInfo'
+  ---@diagnostic disable-next-line:param-type-mismatch
+  if not client or not client:supports_method(method_name) then
+    return vim.notify('Clangd client not found', vim.log.levels.ERROR)
+  end
+  local win = vim.api.nvim_get_current_win()
+  local params = vim.lsp.util.make_position_params(win, client.offset_encoding)
+  ---@diagnostic disable-next-line:param-type-mismatch
+  client:request(method_name, params, function(err, res)
+    if err or #res == 0 then
+      -- Clangd always returns an error, there is no reason to parse it
+      return
+    end
+    local container = string.format('container: %s', res[1].containerName) ---@type string
+    local name = string.format('name: %s', res[1].name) ---@type string
+    vim.lsp.util.open_floating_preview({ name, container }, '', {
+      height = 2,
+      width = math.max(string.len(name), string.len(container)),
+      focusable = false,
+      focus = false,
+      title = 'Symbol Info',
+    })
+  end, bufnr)
+end
+
+---@class ClangdInitializeResult: lsp.InitializeResult
+---@field offsetEncoding? string
+
 local root_files = {
   '.clangd',
   '.clang-tidy',
@@ -15,7 +75,42 @@ function M.launch()
     root_dir = vim.fs.dirname(vim.fs.find(root_files, { upward = true })[1]),
     filetypes = { 'c', 'cpp' },
     single_file_support = true,
-    capabilities = require('mrcjk.lsp').capabilities,
+    capabilities = vim.tbl_deep_extend('force', {
+      textDocument = {
+        completion = {
+          editsNearCursor = true,
+        },
+      },
+      offsetEncoding = { 'utf-8', 'utf-16' },
+    }, require('mrcjk.lsp').capabilities),
+    ---@param init_result ClangdInitializeResult
+    on_init = function(client, init_result)
+      if init_result.offsetEncoding then
+        client.offset_encoding = init_result.offsetEncoding
+      end
+    end,
+    on_attach = function(client, bufnr)
+      local function clangd(opts)
+        local fargs = opts.fargs
+        local cmd = table.remove(fargs, 1)
+
+        if cmd == 'switchSourceHeader' then
+          return switch_source_header(bufnr, client)
+        end
+        if cmd == 'showSymbolInfo' then
+          return symbol_info(bufnr, client)
+        end
+        vim.notify('unknown Clangd command: ' .. cmd, vim.log.levels.ERROR)
+      end
+
+      vim.api.nvim_create_user_command('Clangd', clangd, {
+        nargs = '+',
+        desc = 'Clangd LSP client commands',
+        complete = function(_)
+          return { 'switchSourceHeader', 'showSymbolInfo' }
+        end,
+      })
+    end,
   }
 end
 
