de3d244d5239381c0c94576dffdad89640fb774d
Marc Jakobi <marc@jakobi.dev>
2025-12-09 15:45:17 +0100

    remove Comment.nvim setup call with <Plug> mappings

---
+++ nvim/plugin/comment.lua
@@ -0,0 +1,34 @@
+local cfg = {
+  opleader = { line = 'gc', block = 'gb' },
+  toggler = {
+    line = 'gcc',
+    block = 'gbc',
+  },
+}
+
+vim.keymap.set('n', cfg.opleader.line, '<Plug>(comment_toggle_linewise)', { desc = 'Comment toggle linewise' })
+vim.keymap.set('n', cfg.opleader.block, '<Plug>(comment_toggle_blockwise)', { desc = 'Comment toggle blockwise' })
+
+vim.keymap.set('n', cfg.toggler.line, function()
+  return vim.api.nvim_get_vvar('count') == 0 and '<Plug>(comment_toggle_linewise_current)'
+      or '<Plug>(comment_toggle_linewise_count)'
+end, { expr = true, desc = 'Comment toggle current line' })
+vim.keymap.set('n', cfg.toggler.block, function()
+  return vim.api.nvim_get_vvar('count') == 0 and '<Plug>(comment_toggle_blockwise_current)'
+      or '<Plug>(comment_toggle_blockwise_count)'
+end, { expr = true, desc = 'Comment toggle current block' })
+
+-- VISUAL mode mappings
+vim.keymap.set(
+  'x',
+  cfg.opleader.line,
+  '<Plug>(comment_toggle_linewise_visual)',
+  { desc = 'Comment toggle linewise (visual)' }
+)
+
+vim.keymap.set(
+  'x',
+  cfg.opleader.block,
+  '<Plug>(comment_toggle_blockwise_visual)',
+  { desc = 'Comment toggle blockwise (visual)' }
+)
--- nvim/plugin/setups.lua
+++ nvim/plugin/setups.lua
@@ -2,8 +2,6 @@ local function setup(mod)
   require(mod).setup()
 end
 
-setup('Comment')
-
 if not vim.g.nvim_surround_setup then
   setup('nvim-surround')
   vim.g.nvim_surround_setup = true
