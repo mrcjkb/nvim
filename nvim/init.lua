1f220da3034847adbad5eb308509c3f46fca635d
Marc Jakobi <marc@jakobi.dev>
2025-12-29 13:51:31 +0100

    disable rainbow delimiters in very large files

--- nvim/init.lua
+++ nvim/init.lua
@@ -73,9 +73,9 @@ opt.tags:append { '.tags' }
 
 local function format_diagnostic(prefix, diagnostic)
   local formatted_message = diagnostic
-    .message
-    -- Replace any sequence of whitespace characters (including newlines) with a single space
-    :gsub('%s+', ' ')
+      .message
+      -- Replace any sequence of whitespace characters (including newlines) with a single space
+      :gsub('%s+', ' ')
   return string.format(prefix .. ' %s', formatted_message)
 end
 
@@ -129,17 +129,17 @@ local function cycle_diagnostic_modes(direction)
   local current_config = vim.diagnostic.config() or diagnostic_config
   local modes = {
     { virtual_text = virtual_text_config, virtual_lines = false },
-    { virtual_text = false, virtual_lines = true },
-    { virtual_text = false, virtual_lines = false },
+    { virtual_text = false,               virtual_lines = true },
+    { virtual_text = false,               virtual_lines = false },
   }
 
   local current_mode_index
   for i, mode in ipairs(modes) do
     if
-      (
-        (type(current_config.virtual_text) == 'table' and mode.virtual_text == virtual_text_config)
-        or (current_config.virtual_text == mode.virtual_text)
-      ) and (current_config.virtual_lines == mode.virtual_lines)
+        (
+          (type(current_config.virtual_text) == 'table' and mode.virtual_text == virtual_text_config)
+          or (current_config.virtual_text == mode.virtual_text)
+        ) and (current_config.virtual_lines == mode.virtual_lines)
     then
       current_mode_index = i
       break
@@ -292,3 +292,19 @@ end
 
 -- nvim-ts-context-commentstring
 g.skip_ts_context_commentstring_module = true
+
+g.rainbow_delimiters = {
+  strategy = {
+    [''] = function(bufnr)
+      -- Disabled for very large files, global strategy for large files,
+      -- local strategy otherwise
+      local line_count = vim.api.nvim_buf_line_count(bufnr)
+      if line_count > 10000 then
+        return nil
+      elseif line_count > 1000 then
+        return 'rainbow-delimiters.strategy.global'
+      end
+      return 'rainbow-delimiters.strategy.local'
+    end
+  }
+}
