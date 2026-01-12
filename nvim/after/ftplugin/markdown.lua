234ea291de781a188d3bd9196504dc4e74cf8789
Marc Jakobi <marc@jakobi.dev>
2026-01-12 19:50:46 +0100

    use pandoc with tectonic if available

--- nvim/after/ftplugin/markdown.lua
+++ nvim/after/ftplugin/markdown.lua
@@ -3,5 +3,8 @@ files.treesitter_start()
 
 local pandoc_cmd = 'pandoc'
 if vim.fn.executable(pandoc_cmd) == 1 then
+  if vim.fn.executable('tectonic') == 1 then
+    vim.b.pandoc_compiler_args = '--pdf-backend tectonic'
+  end
   vim.cmd.compiler(pandoc_cmd)
 end
