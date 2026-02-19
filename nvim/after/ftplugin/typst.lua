d894c7b08fe36386b9274a38731194406a1ef640
Marc Jakobi <marc@jakobi.dev>
2026-02-19 19:54:16 +0100

    auto-highlight colors in typst files

--- nvim/after/ftplugin/typst.lua
+++ nvim/after/ftplugin/typst.lua
@@ -1,2 +1,7 @@
 local files = require('mrcjk.files')
 files.treesitter_start()
+
+if not vim.b.highlight_colors_toggled then
+  local hc = require('nvim-highlight-colors')
+  hc.turnOn()
+end
--- nvim/plugin/highlight-colors.lua
+++ nvim/plugin/highlight-colors.lua
@@ -2,19 +2,10 @@ if vim.g.did_highlight_colors_plugin then
   return
 end
 
-local did_setup = false
-
 vim.keymap.set('n', '<leader>ct', function()
-  local hc
-  if not did_setup then
-    hc = require('nvim-highlight-colors')
-    hc.setup {}
-    hc.turnOff()
-    did_setup = true
-  else
-    hc = require('nvim-highlight-colors')
-  end
+  local hc = require('nvim-highlight-colors')
   hc.toggle()
+  vim.b.highlight_colors_toggled = true
 end, { noremap = true, silent = true, desc = 'highlight [c]olors: [t]oggle' })
 
 vim.g.did_highlight_colors_plugin = true
