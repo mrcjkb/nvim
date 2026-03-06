eac444510de232f1843c544244e6aeb144f2ca5b
Marc Jakobi <marc@jakobi.dev>
2026-03-06 22:00:36 +0100

    add graphql-lsp config

--- nvim/after/ftplugin/graphql.lua
+++ nvim/after/ftplugin/graphql.lua
@@ -1,2 +1,13 @@
 local files = require('mrcjk.files')
 files.treesitter_start()
+
+if vim.fn.executable('graphql-lsp') ~= 1 then
+  return
+end
+
+vim.print("graphql")
+
+vim.lsp.start {
+  cmd = { 'graphql-lsp', 'server', '-m', 'stream' },
+  root_dir = vim.fs.dirname(vim.fs.find({ '.graphqlrc*', '.graphql.config.*', 'graphql.config.*', '.git' }, { upward = true })[1]),
+}
