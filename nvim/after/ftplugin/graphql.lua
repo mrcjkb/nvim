e966870714419073a799d2867406e5a91d21b670
Marc Jakobi <marc@jakobi.dev>
2026-03-06 22:08:09 +0100

    formatting

--- nvim/after/ftplugin/graphql.lua
+++ nvim/after/ftplugin/graphql.lua
@@ -5,9 +5,11 @@ if vim.fn.executable('graphql-lsp') ~= 1 then
   return
 end
 
-vim.print("graphql")
+vim.print('graphql')
 
 vim.lsp.start {
   cmd = { 'graphql-lsp', 'server', '-m', 'stream' },
-  root_dir = vim.fs.dirname(vim.fs.find({ '.graphqlrc*', '.graphql.config.*', 'graphql.config.*', '.git' }, { upward = true })[1]),
+  root_dir = vim.fs.dirname(
+    vim.fs.find({ '.graphqlrc*', '.graphql.config.*', 'graphql.config.*', '.git' }, { upward = true })[1]
+  ),
 }
