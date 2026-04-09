5e65d0757989a7f172a751ce210bbe55b1cd0976
Marc Jakobi <marc@jakobi.dev>
2026-04-09 10:11:50 +0200

    enable ui2

--- nvim/init.lua
+++ nvim/init.lua
@@ -167,6 +167,56 @@ vim.cmd([[
   set sessionoptions-=buffers
 ]])
 
+vim.o.cmdheight = 1
+
+-- NOTE: Use g< to expand collapsed messages
+require("vim._core.ui2").enable({
+	enable = true,
+	msg = {
+		targets = {
+			[""] = "msg",
+			empty = "cmd",
+			bufwrite = "msg",
+			confirm = "cmd",
+			emsg = "pager",
+			echo = "msg",
+			echomsg = "msg",
+			echoerr = "pager",
+			completion = "cmd",
+			list_cmd = "pager",
+			lua_error = "pager",
+			lua_print = "msg",
+			progress = "pager",
+			rpc_error = "pager",
+			quickfix = "msg",
+			search_cmd = "cmd",
+			search_count = "cmd",
+			shell_cmd = "pager",
+			shell_err = "pager",
+			shell_out = "pager",
+			shell_ret = "msg",
+			undo = "msg",
+			verbose = "pager",
+			wildlist = "cmd",
+			wmsg = "msg",
+			typed_cmd = "cmd",
+		},
+		cmd = {
+			height = 0.5,
+		},
+		dialog = {
+			height = 0.5,
+		},
+		msg = {
+			height = 0.3,
+			timeout = 4000,
+		},
+		pager = {
+			height = 0.5,
+		},
+	},
+})
+
 -- Plugin settings
 local keymap_opts = { noremap = true, silent = true }
 
